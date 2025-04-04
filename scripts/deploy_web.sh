516542327263be1d018b1031a61e69390f61c353
Josef Hak <jhak@mirantis.com>
2025-04-04 20:57:34 +0200

    Add "site_url" to mkdocs.yml from SITE_URL on web deploy

--- scripts/deploy_web.sh
+++ scripts/deploy_web.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
+if [[ -n "$SITE_URL" ]]; then
+    echo "site url found: $SITE_URL"
+    echo "site_url: $SITE_URL" >> mkdocs.yml
+    KIND_CLUSTER="k0rdent"
+else
+    echo "SITE_URL not found"
+fi
 VERSION="v0.1.0" mike deploy v0.1.0
 VERSION="v0.2.0" mike deploy v0.2.0 latest stable --update-aliases
 mike set-default v0.2.0
